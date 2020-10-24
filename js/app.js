48f72b40ea49c331aaa90fe6864e27c234114b7e
Sofia Bianco <b.sofia2124@gmail.com>
2020-10-24 16:36:47 -0300

    Change var language

--- js/app.js
+++ js/app.js
@@ -1,52 +1,52 @@
 // Variables globales
-const formUI = document.querySelector('#form-tareas');
-const listaTareasUI = document.getElementById('lista-tareas');
-let arrayTareas = [];
+const formUI = document.querySelector('#task-form');
+const taskSectionUI = document.querySelector('#task-section');
+let taskArray = [];
 
 // Funciones
 // Crear Item
-const CrearTareas = (tarea) => {
+const CreateTask = (task) => {
   let item = {
-    tarea: tarea,
-    estado: false
+    task: task,
+    state: false
   }
-  arrayTareas.push(item);
+  taskArray.push(item);
   return item;
 }
 
 // Guardar en el Local Storage
-const GuardarDB = () => {
-  localStorage.setItem('tareas', JSON.stringify(arrayTareas));
+const SaveDB = () => {
+  localStorage.setItem('tasks', JSON.stringify(taskArray));
 
-  MostrarDB();
+  ShowDB();
 }
 
 // Mostrar los datos almacenados en mi localStorage
-const MostrarDB = () => {
-  listaTareasUI.innerHTML = '';
+const ShowDB = () => {
+  taskSectionUI.innerHTML = '';
 
-  arrayTareas = JSON.parse(localStorage.getItem('tareas'));
-  if (arrayTareas === null) {
-    arrayTareas = [];
+  taskArray = JSON.parse(localStorage.getItem('tasks'));
+  if (taskArray === null) {
+    taskArray = [];
   } else {
-    arrayTareas.forEach(elem => {
-      if (elem.estado) {
-        listaTareasUI.innerHTML += `
-        <div class="lista-tareas lista-check">
-          <b>${elem.tarea}</b> <span class="escondido">${elem.estado}</span>
+    taskArray.forEach(elem => {
+      if (elem.state) {
+        taskSectionUI.innerHTML += `
+        <div class="task-list check-list">
+          <b>${elem.task}</b> <span class="hidden">${elem.state}</span>
           <span>
-            <i class="fas fa-trash-alt tareas-icon tarea-check" id="btn-eliminar"></i>
-            <i class="far fa-check-circle tareas-icon tarea-check" id="btn-check"></i>
+            <i class="fas fa-trash-alt task-icon task-check" id="delete-btn"></i>
+            <i class="far fa-check-circle task-icon task-check" id="check-btn"></i>
           </span>
         </div>
         `;
       } else {
-        listaTareasUI.innerHTML += `
-        <div class="lista-tareas">
-          <b>${elem.tarea}</b> <span class="escondido">${elem.estado}</span>
+        taskSectionUI.innerHTML += `
+        <div class="task-list">
+          <b>${elem.task}</b> <span class="hidden">${elem.state}</span>
           <span>
-            <i class="fas fa-trash-alt tareas-icon" id="btn-eliminar"></i>
-            <i class="far fa-check-circle tareas-icon" id="btn-check"></i>
+            <i class="fas fa-trash-alt task-icon" id="delete-btn"></i>
+            <i class="far fa-check-circle task-icon" id="check-btn"></i>
           </span>
         </div>
         `;
@@ -56,54 +56,54 @@ const MostrarDB = () => {
 }
 
 // Eliminar del Local Storage
-const EliminarDB = (tarea) => {
+const DeleteDB = (task) => {
   let idArray;
-  arrayTareas.forEach((elem, id) => {
-    if (elem.tarea === tarea) {
+  taskArray.forEach((elem, id) => {
+    if (elem.task === task) {
       idArray = id;
     }    
   });
 
-  arrayTareas.splice(idArray, 1);
-  GuardarDB();
+  taskArray.splice(idArray, 1);
+  SaveDB();
 }
 
 // Editar el Local Storage
-const EditarDB = (tarea) => {
-  let idArray = arrayTareas.findIndex((elem) => {
-    return (elem.tarea === tarea);
+const EditDB = (task) => {
+  let idArray = taskArray.findIndex((elem) => {
+    return (elem.task === task);
   });
-  console.log(arrayTareas[idArray])
+  console.log(taskArray[idArray])
 
-  arrayTareas[idArray].estado = true;
-  GuardarDB();
+  taskArray[idArray].state = true;
+  SaveDB();
 }
 
 // Eventos
 formUI.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  let tareasUI = document.querySelector('#tareas').value;
-  CrearTareas(tareasUI);
-  GuardarDB();
+  let taskUI = document.querySelector('#task-input').value;
+  CreateTask(taskUI);
+  SaveDB();
 
   formUI.reset();
 });
 
-document.addEventListener('DOMContentLoaded', MostrarDB);
+document.addEventListener('DOMContentLoaded', ShowDB);
 
-listaTareasUI.addEventListener('click', (e) => {
+taskSectionUI.addEventListener('click', (e) => {
   e.preventDefault();
 
   // IMPORTANTE: event.path no está disponible para Firefox
 
-  if (e.target.id === 'btn-check' || e.target.id === 'btn-eliminar') {
-    const tarea = e.path[2].childNodes[1].innerHTML;
-    if (e.target.id === 'btn-eliminar') {
-      EliminarDB(tarea);
+  if (e.target.id === 'check-btn' || e.target.id === 'delete-btn') {
+    const task = e.path[2].childNodes[1].innerHTML;
+    if (e.target.id === 'delete-btn') {
+      DeleteDB(task);
     }
-    if (e.target.id === 'btn-check') {
-      EditarDB(tarea);
+    if (e.target.id === 'check-btn') {
+      EditDB(task);
     }
   }
 
